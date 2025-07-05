d574dfdc2930c048abc6f396b541883d6cea4d5f
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-05 19:13:10 +0300

    Added FileService to handle file uploads and storage on server,DB

---
+++ FileManager.Api/Services/FileService.cs
@@ -0,0 +1,31 @@
+﻿
+namespace FileManager.Api.Services;
+
+public class FileService(IWebHostEnvironment webHostEnvironment,ApplicationDbContext context) : IFileService
+{
+	private readonly string _filePath = $"{webHostEnvironment.WebRootPath}/uploads";
+	private readonly ApplicationDbContext _context = context;
+
+	public async Task<Guid> UploadAsync(IFormFile file, CancellationToken cancellationToken = default)
+	{
+		var randomFileName =Path.GetRandomFileName();
+
+		var uploadedFile = new UploadedFile
+		{
+			FileName = file.FileName,
+			StoredFileName = randomFileName,
+			ContentType = file.ContentType,
+			FileExtension = Path.GetExtension(file.FileName)
+		};
+
+		var path=Path.Combine(_filePath,randomFileName);
+
+		using var stream = File.Create(path);
+		await file.CopyToAsync(stream, cancellationToken);
+
+		await _context.AddAsync(uploadedFile, cancellationToken);
+		await _context.SaveChangesAsync(cancellationToken);
+
+		return uploadedFile.Id;
+	}
+}
