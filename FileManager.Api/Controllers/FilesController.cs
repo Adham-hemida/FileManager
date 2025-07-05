fb7578645ee5dc636d5e1c4438f965a5c346b4be
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-05 19:55:27 +0300

    Added Upload File EndPoint

---
+++ FileManager.Api/Controllers/FilesController.cs
@@ -0,0 +1,18 @@
+﻿using FileManager.Api.Contracts;
+using FileManager.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class FilesController(IFileService fileService) : ControllerBase
+{
+	private readonly IFileService _fileService = fileService;
+
+	[HttpPost("upload")]
+	public async Task<IActionResult> Upload([FromForm] UploadFileRequest request,CancellationToken cancellationToken )
+	{
+		var fileId = await _fileService.UploadAsync(request.File, cancellationToken);
+		return Created();
+	}
+}
