9d7d6ddf6b5ca2361807dd850dceb8ac05378810
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-06 18:53:10 +0300

    Extract 3 Reusable Validator in each Class validator to use it in every where

--- FileManager.Api/Contracts/UploadFileRequestValidator.cs
+++ FileManager.Api/Contracts/UploadFileRequestValidator.cs
@@ -1,30 +1,18 @@
-﻿namespace FileManager.Api.Contracts;
+﻿
+namespace FileManager.Api.Contracts;
 
 public class UploadFileRequestValidator:AbstractValidator<UploadFileRequest>
 {
 	public UploadFileRequestValidator()
 	{
 		RuleFor(x => x.File)
-			.Must((request, context) => request.File.Length <= FileSettings.MaxFileSizeInBytes)
-			.WithMessage($"File size should be less  or equal  {FileSettings.MaxFileSizeInMB} MB.")
-			.When(x => x.File is not null);
+			.SetValidator(new FileSizeValidator());
 
 		RuleFor(x => x.File)
-			.Must((request, context) =>
-			{
-				BinaryReader binary=new(request.File.OpenReadStream());
-				var bytes = binary.ReadBytes(2);
+			.SetValidator(new BlockedSignatureValidator());
 
-				var fileSequenceHex = BitConverter.ToString(bytes);
-				foreach (var signature in FileSettings.BlockSignatures)
-				{
-					if(signature.Equals(fileSequenceHex, StringComparison.OrdinalIgnoreCase))
-						return false;
-				}
-				return true;
-			})
-			.WithMessage("Not allow file content")
-			.When(x => x.File is not null);
+		RuleFor(x => x.File)
+			.SetValidator(new FileNameValidator());
 	}
 }
 
