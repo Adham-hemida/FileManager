a7d3674b03e8c98005d82f6cdd6584702cabb22f
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-06 16:45:27 +0300

    Added Validate File Content

--- FileManager.Api/Contracts/UploadFileRequestValidator.cs
+++ FileManager.Api/Contracts/UploadFileRequestValidator.cs
@@ -5,8 +5,25 @@ public class UploadFileRequestValidator:AbstractValidator<UploadFileRequest>
 	public UploadFileRequestValidator()
 	{
 		RuleFor(x => x.File)
-			.Must((request, context) => request.File.Length <= FileSize.MaxFileSizeInBytes)
-			.WithMessage($"File size should be less  or equal  {FileSize.MaxFileSizeInMB} MB.")
+			.Must((request, context) => request.File.Length <= FileSettings.MaxFileSizeInBytes)
+			.WithMessage($"File size should be less  or equal  {FileSettings.MaxFileSizeInMB} MB.")
+			.When(x => x.File is not null);
+
+		RuleFor(x => x.File)
+			.Must((request, context) =>
+			{
+				BinaryReader binary=new(request.File.OpenReadStream());
+				var bytes = binary.ReadBytes(2);
+
+				var fileSequenceHex = BitConverter.ToString(bytes);
+				foreach (var signature in FileSettings.BlockSignatures)
+				{
+					if(signature.Equals(fileSequenceHex, StringComparison.OrdinalIgnoreCase))
+						return false;
+				}
+				return true;
+			})
+			.WithMessage("Not allow file content")
 			.When(x => x.File is not null);
 	}
 }
